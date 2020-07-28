11756defcb7ab9f7d349f69f7e3d86c64e8721c9
shreya <shreyabhakta@hotmail.com>
2020-07-28 15:18:44 -0500

    finished level 1 tilemap. Added coin picker.

---
+++ New Unity Project/Assets/CoinPicker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPicker : MonoBehaviour {
+	private float coins = 0;
+	
+	private void OnTriggerEnter2D(Collider2D other) {
+		if(other.transform.tag == "Coins") {
+			Destroy(other.gameObject);
+		}
+	}
+
+}
