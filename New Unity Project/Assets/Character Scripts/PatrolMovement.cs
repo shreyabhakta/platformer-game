86ab80c5d6e3ed29224fcda1b257b84064a5d5ad
shreya <shreyabhakta@hotmail.com>
2020-07-29 15:52:04 -0500

    Added enemy movement for saw and mace

---
+++ New Unity Project/Assets/Character Scripts/PatrolMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaceMovement : MonoBehaviour {
+	private float speed = 2f;
+	private bool movingRight = false;
+	
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+	
+    // Update is called once per frame
+    void Update() {
+		if(movingRight) {
+			transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+			transform.localScale = new Vector2 (0.375f,0.375f);
+		}
+		else {
+			transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+			transform.localScale = new Vector2 (-0.375f,0.375f);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.transform.tag == "turn") {
+			if(movingRight == true) {
+				movingRight = false;
+			}
+			else {
+				movingRight = true;
+			}
+		}
+	}
+}
