f4c81104e0453657ffc28566a419e948bdf50651
shreya <shreyabhakta@hotmail.com>
2020-08-03 11:36:50 -0500

    Add coin counter text

--- New Unity Project/Assets/Character Scripts/Collisions.cs
+++ New Unity Project/Assets/Character Scripts/Collisions.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Collisions : MonoBehaviour {
 	public Transform spawnpoint;
 	private float coins = 0;
+	public TextMeshProUGUI coinText;
 	
 	private void OnTriggerEnter2D(Collider2D other) {
 		if(other.transform.tag == "Coins") {
+			coins++;
+			coinText.text = coins.ToString();
 			Destroy(other.gameObject);
 		}
 		if(other.transform.tag == "Water") {
