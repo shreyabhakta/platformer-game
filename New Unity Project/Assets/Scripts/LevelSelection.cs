59594e9cdf4010be7f306bbd16051fe219feb3bc
shreya <shreyabhakta@hotmail.com>
2020-08-11 12:50:30 -0500

    added level 7 and level selection script

---
+++ New Unity Project/Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelection : MonoBehaviour {
+	public Button[] levelButtons;
+	public Image[] gates;
+    // Start is called before the first frame update
+    void Start() {
+		int levelAt = PlayerPrefs.GetInt("levelAt", 1);
+        
+		for(int i = 0; i < levelButtons.Length; i++) {
+			if( i + 1 > levelAt) {
+				levelButtons[i].interactable = false;
+				gates[i].enabled = true;
+			}
+			else {
+				gates[i].enabled = false;
+			}
+		}
+    }
+
+
+}
